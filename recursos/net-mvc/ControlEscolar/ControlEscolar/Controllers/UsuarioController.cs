bcef899ce9f0a002554d745251c1d5f47981f2af
Miguel Torres <mafairnet@gmail.com>
2023-12-09 20:49:54 -0500

    Se suben archvos de primera sesion de .net mvc
    Se suben archvos de primera sesion de .net mvc

---
+++ recursos/net-mvc/ControlEscolar/ControlEscolar/Controllers/UsuarioController.cs
@@ -0,0 +1,94 @@
+﻿using ControlEscolar.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlEscolar.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Usuario/Create
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        // POST: Usuario/Create
+        [HttpPost]
+        public ActionResult Add(Usuario usuario)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuario/Edit/5
+        public ActionResult Edit(int ID)
+        {
+            ViewData["userId"] = ID;
+
+            var usuario = new Usuario();
+            usuario.ID = ID;
+            usuario.FirstName = "Miguel";
+            usuario.SecondName = "Torres";
+            usuario.Username = "mafairnet";
+
+            ViewData["user"] = usuario;
+
+            return View();
+        }
+
+        // POST: Usuario/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Usuario usuario)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuario/Delete/5
+        public ActionResult Delete(int ID)
+        {
+            return View();
+        }
+
+        // POST: Usuario/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Usuario usuario)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
